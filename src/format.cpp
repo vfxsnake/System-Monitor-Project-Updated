c39e3eeed5cd715577c2dc92ec5d3bbe690bb301
vfxnake <jazzjoe@gmail.com>
2022-09-26 22:51:44 -0400

    ZeroPadding implemented for Fromat.h

--- src/format.cpp
+++ src/format.cpp
@@ -5,9 +5,15 @@ using std::string;
 
 string Format::ElapsedTime(long seconds) 
 {
-    int current_seconds = int(seconds % 60);
-    int current_minutes = (seconds/60) % 60;
-    int curret_hours = seconds/3600;
-    string string_formatted = std::to_string(curret_hours) + ":" + std::to_string(current_minutes) + ":" + std::to_string(current_seconds);
+    std::string current_seconds = std::to_string(seconds % 60);
+    std::string current_minutes = std::to_string((seconds/60) % 60);
+    std::string curret_hours = std::to_string(seconds/3600);
+    
+    string string_formatted = PadZeros(curret_hours) + ":" + PadZeros(current_minutes) + ":" + PadZeros(current_seconds);
     return string_formatted; 
+}
+
+string Format::PadZeros(std::string &source, int padding_size)
+{
+    return source.insert(0, padding_size - source.length(), '0');
 }
